09889faacecf049d36e7992ca7ca2f15dd47b2c7
sniffsnirr <alfo12@mail.ru>
2022-02-15 14:40:51 +0300

    zadanie 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 "scriptstrict";
 let numberOfFilms=prompt("Сколько фильмов Вы уже посмотрели?",0);
 console.info(`Пользователь посмотрел ${numberOfFilms} фильма(ов).`);
+//------------------------------------------------------------------------
+let personalMovieDB={
+   count:   numberOfFilms,
+   movies:  {},
+   actors:  {},
+   genres:  [],
+   privat:false
+};
+//------------------------------------------------------------------------
