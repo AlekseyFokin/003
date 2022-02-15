f0c952093abf3fb15396f846f722264562da4e61
sniffsnirr <alfo12@mail.ru>
2022-02-15 15:17:25 +0300

    zadanie 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "scriptstrict";
-let numberOfFilms=prompt("Сколько фильмов Вы уже посмотрели?",0);
-console.info(`Пользователь посмотрел ${numberOfFilms} фильма(ов).`);
+let numberOfFilms=+prompt("Сколько фильмов Вы уже посмотрели?",0);
+console.log(`Пользователь посмотрел ${numberOfFilms} фильма(ов).`);
 //------------------------------------------------------------------------
 let personalMovieDB={
    count:   numberOfFilms,
@@ -10,3 +10,12 @@ let personalMovieDB={
    privat:false
 };
 //------------------------------------------------------------------------
+let quation1=prompt("Один из последних просмотренных фильмов?","Robocop");
+let quation2=prompt("На сколько оцените его?",0);
+let quation3=prompt("Один из последних просмотренных фильмов?","Robocop");
+let quation4=prompt("На сколько оцените его?",0);
+
+personalMovieDB.movies[quation1]=quation2;
+personalMovieDB.movies[quation3]=quation4;
+
+console.log(personalMovieDB);
